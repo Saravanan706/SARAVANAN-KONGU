0bb3de26d37a126eb301e137717efdbe57c353dd
Saravavan706 <30343017+Saravavan706@users.noreply.github.com>
2017-07-21 17:03:11 +0530

    Create POSITIVEN.c

---
+++ POSITIVEN.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("\n Enter a value);
+scanf("%d",&a);
+if(a>0)
+printf("\n Number is positive");
+if else(a<0)
+printf("\n Number is negative");
+else
+printf("\n Number is zero");
+}
