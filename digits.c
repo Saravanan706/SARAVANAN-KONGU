d03f43878d78cec4c60e7783a144853008811647
Saravanan T N <30343017+Saravavan706@users.noreply.github.com>
2017-08-29 16:12:36 +0530

    Update digits.c

--- digits.c
+++ digits.c
@@ -4,10 +4,10 @@ void main()
 int a,b=0;
 printf("\n Enter the number");
 scanf("%d",&a);
-if(a=!0)
+if(a!=0)
 {
 a=a/10;
 ++b;
 }
-printf("\n No of digit="%d",b);
+printf("\n No of digit=%d",b);
 }
